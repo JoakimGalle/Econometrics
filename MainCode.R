4f68a549e4bf8c45b57322499fc830cf2f7bd84a
JoakimGalle <joakim.galle@hotmail.com>
2021-03-24 00:09:04 +0100

    structuring

---
+++ MainCode.R
@@ -0,0 +1,51 @@
+####################################################################
+####################################################################
+###                   Case Econometrics                          ###
+### Bachelor of Science in Economics and Business Engineering    ###
+###         Academic year 2021-21 Ghent University                ###
+####################################################################
+####################################################################
+
+####################################################################
+### In this case we want to estimate the impact of trade         ###
+###                 on GDP per worker                             ###   
+####################################################################
+
+
+####################################################################
+## Load required packages: this needs to be done every time you
+## close an R-session
+####################################################################
+library(pastecs)  ## Descriptive Statistics
+library(psych)    ## Correlation plot
+library(moments)  ## Testing for Normality check up
+library(lmtest)   ## Tests for heteroscedasticity
+library(sandwich) ## Newey-West HAC estimators
+library(AER)      ## 2SLS
+library(stargazer)## Stargazer
+library(orcutt)   ## Cochrane-Orcutt EGLS
+library(nlme)     ## Linear models with autocorrelated error terms
+library(openxlsx) ## To read an excel xlsx data file
+library(EconometricsUGent)  ## Additional functions
+
+
+####################################################################
+## Import data set
+####################################################################
+data = read.xlsx("Trade.xlsx", colNames = TRUE)
+
+####################################################################
+## Determine number of observations and number of variables
+####################################################################
+dim(data)             ## Number of variables and sample size
+n = length(data[,1])  ## Sample size
+
+####################################################################
+## Define variables 
+####################################################################
+gdp   = log(data$`GDP.per.worker.(in.US.dollars)`)  # ln GDP per worker
+area  = log(data$`Area.(in.sq.miles)`)              # ln Area
+pop   = log(data$`Workers.(in.thousands)`)          # ln Population
+trade = data$Trade                                  # Trade share
+landlocked = factor(data$Landlocked.dummy)          # Landlocked dummy
+neighbors = data$Number.of.neighboring.countries    # Number of neighboring countries 
--- User-specific.R
+++ User-specific.R
@@ -1,16 +1,3 @@
-####################################################################
-####################################################################
-###                   Case Econometrics                          ###
-### Bachelor of Science in Economics and Business Engineering    ###
-###         Academic year 2021-21 Ghent University                ###
-####################################################################
-####################################################################
-
-####################################################################
-### In this case we want to estimate the impact of trade         ###
-###                 on GDP per worker                             ###   
-####################################################################
-
 rm(list = ls())   # Clear workspace 
 
 ####################################################################
@@ -88,42 +75,3 @@ install.packages("C:\\users\\ymeersch\\Desktop\\EconometricsUGent_1.0.tar.gz", s
 
 ## For Mac
 ## install.packages("H:/Desktop/EconometricsUGent_1.0.tar.gz", type="source",repos = NULL, lib = output) 
-
-
-####################################################################
-## Load required packages: this needs to be done every time you
-## close an R-session
-####################################################################
-library(pastecs)  ## Descriptive Statistics
-library(psych)    ## Correlation plot
-library(moments)  ## Testing for Normality check up
-library(lmtest)   ## Tests for heteroscedasticity
-library(sandwich) ## Newey-West HAC estimators
-library(AER)      ## 2SLS
-library(stargazer)## Stargazer
-library(orcutt)   ## Cochrane-Orcutt EGLS
-library(nlme)     ## Linear models with autocorrelated error terms
-library(openxlsx) ## To read an excel xlsx data file
-library(EconometricsUGent)  ## Additional functions
-
-
-####################################################################
-## Import data set
-####################################################################
-data = read.xlsx("Trade.xlsx", colNames = TRUE)
-
-####################################################################
-## Determine number of observations and number of variables
-####################################################################
-dim(data)             ## Number of variables and sample size
-n = length(data[,1])  ## Sample size
-
-####################################################################
-## Define variables 
-####################################################################
-gdp   = log(data$`GDP.per.worker.(in.US.dollars)`)  # ln GDP per worker
-area  = log(data$`Area.(in.sq.miles)`)              # ln Area
-pop   = log(data$`Workers.(in.thousands)`)          # ln Population
-trade = data$Trade                                  # Trade share
-landlocked = factor(data$Landlocked.dummy)          # Landlocked dummy
-neighbors = data$Number.of.neighboring.countries    # Number of neighboring countries 
